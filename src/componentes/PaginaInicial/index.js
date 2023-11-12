775ce1e0a44c93dd0dbdbee2707a4f2de1820203
Osvaldo Holanda <osvaldolimah@gmail.com>
2023-11-12 09:49:26 -0300

    linkando a pagina inicial

--- src/componentes/PaginaInicial/index.js
+++ src/componentes/PaginaInicial/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 const PaginaInicial = () => (
-  <div>
-    <h1>Escolha seu caminho em JavaScript</h1>
+ <div>
+  <h1>Escolha seu caminho em JavaScript</h1>
 
-    <div className="conteudo-opcoes">
-      <a className="opcao-front-end" href='/front-end'>Front-end</a>
-      <a className="opcao-back-end" href='/back-end'>Back-end</a>
-    </div>
-    
-  </div>
+   <div className="conteudo-opcoes">
+    <Link className="opcao-front-end" to='/front-end'>Front-end</Link>
+    <Link className="opcao-back-end" to='/back-end'>Back-end</Link>
+   </div>
+
+ </div>
 );
 
 export default PaginaInicial;
